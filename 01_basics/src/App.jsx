e8ad41ff7dfb5d72cb69a1b99ec47d9e76bfe911
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-05-25 20:41:02 +0500

    Updated

--- 01_basics/src/App.jsx
+++ 01_basics/src/App.jsx
@@ -1,9 +1,12 @@
-
+import Biryani from "./biryani"
 function App() {
   
 
   return (
+   <> 
    <h1>Hello World | Created By Mesum Bin Shaukat</h1>
+   <Biryani />
+   </>
   )
 }
 
---
+++ 01_basics/src/biryani.jsx
@@ -0,0 +1,7 @@
+function Biryani (){ // Always capitalize the function/component name
+    return (
+        <h3>Buy me a plate of Biryani</h3>
+    )
+}
+
+export default Biryani
