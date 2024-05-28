761875430b1a75178c0a66294c27c3476263fd11
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-05-28 17:31:43 +0500

    React Router Practice With Small Project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold bg-yellow-400 text-center'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
